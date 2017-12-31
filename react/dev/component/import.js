f57fabde76ce73ae5b12ff0715b13e429369fc2e
panchong <953456262@qq.com>
2017-12-31 14:37:30 +0800

    上传打包文件

--- react/dev/component/import.js
+++ react/dev/component/import.js
@@ -1,12 +1,12 @@
 // import less
 import '../../less/main.less';
-import '../less/flex.less';
-import './waimai.less';
-import './selectHard.less';
-import './award.less';
+import '../../less/flex.less';
+import '../waimai.less';
+import '../selectHard.less';
+import '../award.less';
 
 // import image
-import '../images/favicon.png';
-import '../images/waimai.gif';
-import '../images/workEnd.gif';
-import '../images/defaultImage.png';
+import '../../images/favicon.png';
+import '../../images/waimai.gif';
+import '../../images/workEnd.gif';
+import '../../images/defaultImage.png';
